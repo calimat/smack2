f99350b52114d3a6bbdbb97063110fd8bf4bb7f6
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-01-31 22:35:43 -0500

    Created LoginVC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Ricardo Herrera Petit on 1/31/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  Smack
+//
+//  Created by Ricardo Herrera Petit on 1/31/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
