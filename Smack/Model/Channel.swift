a078ff9f1818f270b7b973442a152f7be9793579
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-02-06 23:47:50 -0500

    Starting getting the channels from the API

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Ricardo Herrera Petit on 2/6/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private (set) var channelTitle: String!
+    public private (set) var channelDescription: String!
+    public private (set) var id: String!
+}
