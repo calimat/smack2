1e33facea29bf2f3c5df19701461ce8998ca9781
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-02-08 23:30:01 -0500

    Add channel vc

---
+++ Smack/Controller/AddChannelVC.swift
@@ -0,0 +1,44 @@
+//
+//  AddChannelVC.swift
+//  Smack
+//
+//  Created by Ricardo Herrera Petit on 2/8/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var channDesc: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor: smackPurplerPlaceholder])
+        channDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor: smackPurplerPlaceholder])
+        
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+}
