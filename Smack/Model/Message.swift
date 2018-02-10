e5634a45414e41e2f791c616ed16925985f3b996
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-02-09 22:26:06 -0500

    Service to get Messages

---
+++ Smack/Model/Message.swift
@@ -0,0 +1,22 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by Ricardo Herrera Petit on 2/9/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    public private (set) var message:String!
+    public private (set) var userName:String!
+    public private (set) var channelId:String!
+    public private (set) var userAvatar:String!
+    public private (set) var userAvatarColor:String!
+    public private (set) var id:String!
+    public private (set) var timeStamp:String!
+    
+    
+}
